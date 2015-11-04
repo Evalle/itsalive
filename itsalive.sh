6ee49ef2a0893201435f698c2a18540d7262f06e
evalle <shmarnev@gmail.com>
2015-11-04 17:00:26 +0100

    Change 'is up' to 'is alive' for great justice!
    Signed-off-by: evalle <shmarnev@gmail.com>

--- itsalive.sh
+++ itsalive.sh
@@ -5,7 +5,7 @@ NETWORK=$1
 itsalive()
 {
     ping -c 1 $1 > /dev/null
-    [ $? -eq 0 ] && echo Node with IP: $i is up.
+    [ $? -eq 0 ] && echo Node with IP: $i is alive!
 }
 
 for i in $NETWORK.{1..254} 
